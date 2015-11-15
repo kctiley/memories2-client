e0eaa5a3b3585cba322485d2f8f3582d3e85630b
Klint Tiley <kctiley@gmail.com>
2015-11-15 10:07:42 -0700

    Form now working to submit to database. Route on server side working, but does not yet add new memory to DOM until page refreshed.

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,41 @@
-app.controller('HomeController', function($scope, $http) {
-  $http.get("http://g12-klint-tiley.cfapps.io/api/v1/memories").then(function(result){
-    
-    $scope.memories = result.data;
+// app.controller('HomeController', function($scope, $http) {
+//   $http.get("http://g12-klint-tiley.cfapps.io/api/v1/memories").then(function(result){
+//     $scope.memories = result.data;
+//   });
+// }); 
+
+app.controller('HomeController', [ '$scope', '$http', function($scope, $http ) {
+  $scope.memories = [];
+  $http.get("http://g12-klint-tiley.cfapps.io/api/v1/memories").then(function(response){
+    for (var i in response.data){
+      $scope.memories.push(response.data[i]);
+    }
   });
-});  
+
+  $scope.submit = function(){
+    var memoryObj = 
+      {
+        "data": {
+          "type": "memory",
+          "attributes": {
+            "old_days": "string",
+            "these_days": "string",
+            "year": 0
+          }
+        }
+      };
+      console.log($scope);
+    memoryObj.data.attributes.old_days = $scope.old_days;
+    memoryObj.data.attributes.these_days = $scope.these_days;
+    memoryObj.data.attributes.year = $scope.year;
+
+    $scope.memories.push(memoryObj);
+
+    $http.post('http://g12-klint-tiley.cfapps.io/api/v1/memories', memoryObj).then(function(response){
+      },function(){
+          console.log('Error in post')
+    });
+    $scope.memoryObj = {};
+  };
+
+}])
